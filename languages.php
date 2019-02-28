eef6817708bc4b5f6cdcf42b67138fc6f28a866d
Martin Vician <info@vician.cz>
2019-02-28 17:02:42 +0000

    sorted strings

--- languages.php
+++ languages.php
@@ -42,7 +42,7 @@ function language_review() {
 	foreach ($translates as $lang => $strings) {
 		echo "<th>".$lang."</th>";
 	}
-	foreach ($language_strings as $string) {
+	foreach (sort($language_strings) as $string) {
 		if($string == "") continue;
 		if(is_numeric($string)) continue;
 		if(in_array($string,array("st","nd","rd","th"))) continue;
@@ -81,7 +81,7 @@ function language_review() {
 	foreach ($translates as $lang => $strings) {
 		if ($lang == "en") continue;
 		echo "\$$lang = array(\n";
-		foreach ($language_strings as $string) {
+		foreach (sort($language_strings) as $string) {
 			if($string == "") continue;
 			if(is_numeric($string)) continue;
 			if(in_array($string,array("st","nd","rd","th"))) continue;
