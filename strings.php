325473d6d32c338632d18959ddf2eb974c35676a
Martin Vician <info@vician.cz>
2019-02-28 16:35:30 +0000

    fixup! strings separated

---
+++ strings.php
@@ -0,0 +1,41 @@
+<?php
+
+$language_strings = array("");
+$en = array();
+$cs = array(
+	"I didn't lock my computer again!" => "Zase jsem si nezamkl(a) počítač!"
+	"Toggle navigation" => "Zobrazit/skrýt menu"
+	"Why" => "Proč"
+	"How to use" => "Jak používat"
+	"About" => "O nás"
+	"I didn't lock my computer again!" => "Zase jsem si nezamkl(a) počítač!"
+	"hit" => "zásah"
+	"Why" => "Proč"
+	"should I lock my computer?" => "bych si měl(a) zamykat počítač?"
+	"Because you should secure data on laptop's disk and in memory. You also should protect your device from unauthorized installation of programs - e.g. keyloggers, program for botnets and mining virtual currencies. Don't forget that you are the product for a lot of companies nowadays." => ""
+	"How to use" => "Jak používat"
+	"this page?" => "bych si měl(a) zamykat počítač?"
+	"When you see unlocked device of your colleague or friend open browser on it and open this webpage. The hints increase each visit of this webpage." => ""
+	"About" => "O nás"
+	"and contact" => "a kontakt"
+	"We would like to teach people to lock their computers while they are away and improve security." => "Chceme rozšířit povědomí o nutnosti zamykat svá zařízení, když je nemáme pod dohledem."
+	"Authors" => "Autoři"
+	"and" => "a"
+	"Code released under the" => "Zdrojový kód zvěřejněn pod"
+	"license" => "licencí"
+	"Source code" => "Zdrojový kód"
+	"Theme" => "HTML šablona"
+	"Contribute" => "Jak se zapojit"
+	"Use and show your friends and colleagues." => "Používejte tento web a ukažte jej svým známym a kolegům."
+	"Translate this page to your language." => "Pomozte nám přeložit tuto stránku do vašeho jazyka."
+	"Code released under the" => "Zdrojový kód zvěřejněn pod"
+	"license" => "licencí"
+	"Strings for translation" => "Texty pro překlad"
+	"Not used strings" => "Nepoužité řetězce"
+	"PHP export" => "Export do PHP"
+);
+
+$translates = array(
+	'en' => $en, // English is default
+	'cs' => $cs,
+);
